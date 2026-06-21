e0fb79d9485b28a0baab9827cb1cb3438fc77b6e
Guilherme Rondon <worktradestudygrra@gmail.com>
2026-06-20 21:44:36 -0300

    fix(test): alterar sintaxe de skip de xdescribe para describe.skip adequando ao vitest

--- src/app/components/terminal/terminal.component.spec.ts
+++ src/app/components/terminal/terminal.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TerminalComponent } from './terminal.component';
 
-xdescribe('TerminalComponent', () => {
+describe.skip('TerminalComponent', () => {
   let component: TerminalComponent;
   let fixture: ComponentFixture<TerminalComponent>;
 
